46e4db9b1fa5f0c4b0be6d41537f5084186ef17d
Neha <nehaai2103@gmail.com>
2026-05-24 12:19:35 +0530

    fix email port 587

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const supabase = createClient(
 
 // Nodemailer
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host:"smtp.gmail.com",
+  port:587,
+  secure:false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
